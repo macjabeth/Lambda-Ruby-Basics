34794d0c8a7878b915b89f5e71bf51991bd4d378
macjabeth <macbethjonathan@gmail.com>
2019-09-07 16:44:37 -0400

    complete data setup

---
+++ candidates.rb
@@ -0,0 +1,32 @@
+jane = {
+  first_name: 'Jane',
+  last_name: 'Doe',
+  level: :senior_level,
+  age: 30,
+  occupation: 'Software Developer',
+  employed: true,
+  current_salary: 125_000.00,
+  languages: %w[Ruby JavaScript Go]
+}
+
+sara = {
+  first_name: 'Sara',
+  last_name: 'Smith',
+  level: :mid_level,
+  age: 26,
+  occupation: 'Software Developer',
+  employed: true,
+  current_salary: 80_000.00,
+  languages: %w[Ruby Python]
+}
+
+jason = {
+  first_name: 'Jason',
+  last_name: 'Gray',
+  level: :entry_level,
+  age: 23,
+  occupation: 'Student',
+  employed: false,
+  current_salary: 0.00,
+  languages: %w[C++ Python]
+}
